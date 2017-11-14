557c632b9a7296205aca8ced34ff2407ed6b0de4
hiten <hitensethiya@hotmail.com>
2017-11-14 10:14:45 +0530

    removed cscope files

---
+++ src/vm/build/vm/frame.d
@@ -0,0 +1,4 @@
+vm/frame.o: ../../vm/frame.c ../../threads/malloc.h ../../lib/debug.h \
+  ../../lib/stddef.h ../../threads/palloc.h ../../threads/synch.h \
+  ../../lib/kernel/list.h ../../lib/stdbool.h ../../lib/stdint.h \
+  ../../threads/thread.h ../../threads/synch.h ../../vm/frame.h
